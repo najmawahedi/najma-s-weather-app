176b4ee5ab003fa63d30ce782d5185c3db3c4363
najmawahedi <164718474+najmawahedi@users.noreply.github.com>
2024-07-26 18:16:46 +0430

    adding more functionality to the code and fixing the five days forecast

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function changewethear(response) {
   moist.innerHTML = response.data.temperature.humidity;
 
   let icon = document.querySelector("#icon");
-  icon.innerHTML = `<img src="${response.data.condition.icon_url}" width="80px" alt="Weather icon" />`;
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" width="100px" alt="Weather icon" />`;
 
   let localTime = new Date(response.data.time * 1000);
 
@@ -55,8 +55,7 @@ function changewethear(response) {
   let timee = document.querySelector("#timee");
   timee.innerHTML = `${day}, ${datee} ${month}, ${year}`;
 
-  let time = document.querySelector("#time");
-  time.innerHTML = `${hours}:${minutesfixed(minutes)}`;
+  forecast(response.data.city);
 }
 
 function minutesfixed(minutes) {
@@ -84,5 +83,163 @@ function cityname(event) {
 function forecast(city) {
   let forecastapi = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=9d0d4df38e762b48ade23c7aefo5tbbd&units=metric`;
   console.log(forecastapi);
+  axios.get(forecastapi).then(forecastwethear);
 }
-forecast("paris");
+
+function forecastwethear(response) {
+  console.log(response.data);
+  let high = document.querySelector("#high");
+  high.innerHTML = Math.round(response.data.daily[0].temperature.maximum);
+  let low = document.querySelector("#low");
+  low.innerHTML = Math.round(response.data.daily[0].temperature.minimum);
+
+  let localTime = new Date(response.data.daily[1].time * 1000);
+
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let firstday = weekdays[localTime.getDay()];
+  let firstdayname = document.querySelector("#firstdayname");
+
+  firstdayname.innerHTML = firstday;
+
+  let firstdaytemphigh = document.querySelector("#firstdaytemphigh");
+  firstdaytemphigh.innerHTML = Math.round(
+    response.data.daily[1].temperature.maximum
+  );
+  let firstdaytemplow = document.querySelector("#firstdaytemplow");
+  firstdaytemplow.innerHTML = Math.round(
+    response.data.daily[1].temperature.minimum
+  );
+
+  let firstdaycondition = document.querySelector("#firstdaycondition");
+  firstdaycondition.innerHTML = response.data.daily[1].condition.description;
+
+  let firstdaywindspeed = document.querySelector("#firstdaywindspeed");
+  firstdaywindspeed.innerHTML = response.data.daily[1].wind.speed;
+
+  let firstdaymoist = document.querySelector("#firstdaymoist");
+  firstdaymoist.innerHTML = response.data.daily[1].temperature.humidity;
+
+  let firstdayicon = document.querySelector("#firstdayicon");
+  firstdayicon.innerHTML = `<img src="${response.data.daily[1].condition.icon_url}" width="70px" hight="30px" alt="Weather icon" />`;
+
+  let localTime1 = new Date(response.data.daily[2].time * 1000);
+
+  let secondday = weekdays[localTime1.getDay()];
+  let seconddayname = document.querySelector("#seconddayname");
+
+  seconddayname.innerHTML = secondday;
+
+  let seconddaytemphigh = document.querySelector("#seconddaytemphigh");
+  seconddaytemphigh.innerHTML = Math.round(
+    response.data.daily[2].temperature.maximum
+  );
+  let seconddaytemplow = document.querySelector("#seconddaytemplow");
+  seconddaytemplow.innerHTML = Math.round(
+    response.data.daily[2].temperature.minimum
+  );
+
+  let seconddaycondition = document.querySelector("#seconddaycondition");
+  seconddaycondition.innerHTML = response.data.daily[2].condition.description;
+
+  let seconddaywindspeed = document.querySelector("#seconddaywindspeed");
+  seconddaywindspeed.innerHTML = response.data.daily[2].wind.speed;
+
+  let seconddaymoist = document.querySelector("#seconddaymoist");
+  seconddaymoist.innerHTML = response.data.daily[2].temperature.humidity;
+
+  let seconddayicon = document.querySelector("#seconddayicon");
+  seconddayicon.innerHTML = `<img src="${response.data.daily[2].condition.icon_url}" width="70px" hight="30px" alt="Weather icon" />`;
+
+  let localTime2 = new Date(response.data.daily[3].time * 1000);
+
+  let thirdday = weekdays[localTime2.getDay()];
+  let thirddayname = document.querySelector("#thirddayname");
+
+  thirddayname.innerHTML = thirdday;
+
+  let thirdaytemphigh = document.querySelector("#thirdaytemphigh");
+  thirdaytemphigh.innerHTML = Math.round(
+    response.data.daily[3].temperature.maximum
+  );
+  let thirddaytemplow = document.querySelector("#thirddaytemplow");
+  thirddaytemplow.innerHTML = Math.round(
+    response.data.daily[3].temperature.minimum
+  );
+
+  let thirddaycondition = document.querySelector("#thirddaycondition");
+  thirddaycondition.innerHTML = response.data.daily[3].condition.description;
+
+  let thirddaywindspeed = document.querySelector("#thirddaywindspeed");
+  thirddaywindspeed.innerHTML = response.data.daily[3].wind.speed;
+
+  let thirddaymoist = document.querySelector("#thirddaymoist");
+  thirddaymoist.innerHTML = response.data.daily[3].temperature.humidity;
+
+  let thirddayicon = document.querySelector("#thirddayicon");
+  thirddayicon.innerHTML = `<img src="${response.data.daily[3].condition.icon_url}" width="70px" hight="30px" alt="Weather icon" />`;
+
+  let localTime3 = new Date(response.data.daily[4].time * 1000);
+
+  let forthday = weekdays[localTime3.getDay()];
+  let forthdayname = document.querySelector("#forthdayname");
+
+  forthdayname.innerHTML = forthday;
+
+  let forthdaytemphigh = document.querySelector("#forthdaytemphigh");
+  forthdaytemphigh.innerHTML = Math.round(
+    response.data.daily[4].temperature.maximum
+  );
+  let forthdaytemplow = document.querySelector("#forthdaytemplow");
+  forthdaytemplow.innerHTML = Math.round(
+    response.data.daily[4].temperature.minimum
+  );
+
+  let forthdaycondition = document.querySelector("#forthdaycondition");
+  forthdaycondition.innerHTML = response.data.daily[4].condition.description;
+
+  let forthdaywindspeed = document.querySelector("#forthdaywindspeed");
+  forthdaywindspeed.innerHTML = response.data.daily[4].wind.speed;
+
+  let forthdaymoist = document.querySelector("#forthdaymoist");
+  forthdaymoist.innerHTML = response.data.daily[4].temperature.humidity;
+
+  let forthdayicon = document.querySelector("#forthdayicon");
+  forthdayicon.innerHTML = `<img src="${response.data.daily[4].condition.icon_url}" width="70px" hight="30px" alt="Weather icon" />`;
+
+  let localTime4 = new Date(response.data.daily[5].time * 1000);
+
+  let fifthday = weekdays[localTime4.getDay()];
+  let fifthdayname = document.querySelector("#fifthdayname");
+
+  fifthdayname.innerHTML = fifthday;
+
+  let fifthdaytemphigh = document.querySelector("#fifthdaytemphigh");
+  fifthdaytemphigh.innerHTML = Math.round(
+    response.data.daily[5].temperature.maximum
+  );
+  let fifthdaytemplow = document.querySelector("#fifthdaytemplow");
+  fifthdaytemplow.innerHTML = Math.round(
+    response.data.daily[5].temperature.minimum
+  );
+
+  let fifthdaycondition = document.querySelector("#fifthdaycondition");
+  fifthdaycondition.innerHTML = response.data.daily[5].condition.description;
+
+  let fifthdaywindspeed = document.querySelector("#fifthdaywindspeed");
+  fifthdaywindspeed.innerHTML = response.data.daily[5].wind.speed;
+
+  let fifthdaymoist = document.querySelector("#fifthdaymoist");
+  fifthdaymoist.innerHTML = response.data.daily[5].temperature.humidity;
+
+  let fifthdayicon = document.querySelector("#fifthdayicon");
+  fifthdayicon.innerHTML = `<img src="${response.data.daily[5].condition.icon_url}" width="70px" hight="30px" alt="Weather icon" />`;
+}
